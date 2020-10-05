e85a6b656df67fd43b79eeb21f09fa263b5e8966
lagniez <jmlagniez@gmail.com>
2020-10-05 19:01:58 +0200

    clean, and ready for parsing option.

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,20 +3,6 @@
 
 #include <boost/program_options.hpp>
 
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boost/archive/binary_oarchive.hpp>
-#include <boost/archive/binary_iarchive.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/multiprecision/cpp_bin_float.hpp>
-#include <boost/math/special_functions/gamma.hpp>
-#include <boost/multiprecision/cpp_bin_float.hpp>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <iterator>
-
-using namespace boost::program_options;
-
 void on_age(int age)
 {
   std::cout << "On age: " << age << '\n';
@@ -30,24 +16,21 @@ int main(int argc, char** argv)
 {
   try
     {
-      options_description desc{"Options"};
+      boost::program_options::options_description desc{"Options"};
       desc.add_options()
         ("help,h", "Help screen")
-        ("pi", value<float>()->default_value(3.14f), "Pi")
-        ("age", value<int>()->notifier(on_age), "Age");
+        ("pi", boost::program_options::value<float>()->default_value(3.14f), "Pi")
+        ("age", boost::program_options::value<int>()->notifier(on_age), "Age");
 
-      variables_map vm;
-      store(parse_command_line(argc, argv, desc), vm);
-      notify(vm);
+      boost::program_options::variables_map vm;
+      boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+      boost::program_options::notify(vm);
 
-      if (vm.count("help"))
-        std::cout << desc << '\n';
-      else if (vm.count("age"))
-        std::cout << "Age: " << vm["age"].as<int>() << '\n';
-      else if (vm.count("pi"))
-        std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
+      if (vm.count("help")) std::cout << desc << '\n';
+      else if (vm.count("age")) std::cout << "Age: " << vm["age"].as<int>() << '\n';
+      else if (vm.count("pi")) std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
     }
-  catch (const error &ex)
+  catch (const boost::program_options::error &ex)
     {
       std::cerr << ex.what() << '\n';
     }
