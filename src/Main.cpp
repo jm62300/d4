bab00ef7ad9bdb5ada5e52162383b9309d426bbe
Lagniez Jean-Marie <jmarie.lagniez@univ-artois.fr>
2021-04-15 21:42:13 +0200

    remove one level of makefile

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,26 +1,25 @@
 /*
-* d4
-* Copyright (C) 2020  Univ. Artois & CNRS
-* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * d4
+ * Copyright (C) 2020  Univ. Artois & CNRS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <iostream>
-#include <cassert>
-#include <vector>
 #include <boost/program_options.hpp>
-
+#include <cassert>
+#include <iostream>
+#include <vector>
 
 #include "src/methods/MethodManager.hpp"
 
@@ -29,8 +28,7 @@
 /**
    The main function!
 */
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   namespace po = boost::program_options;
   po::options_description desc{"Options"};
   desc.add_options()
@@ -40,29 +38,26 @@ int main(int argc, char** argv)
   po::variables_map vm;
   po::store(parse_command_line(argc, argv, desc), vm);
 
-  try
-  {    
+  try {
     po::notify(vm);
-  }
-  catch (const po::error &ex)
-  {
+  } catch (const po::error &ex) {
     std::cerr << ex.what() << '\n';
     exit(1);
   }
 
   // help or problem with the command line
-  if (vm.count("help") || !vm.count("input"))
-  {
-    if(!vm.count("help"))
+  if (vm.count("help") || !vm.count("input")) {
+    if (!vm.count("help"))
       std::cout << "Some parameters are missing, please read the README\n";
     std::cout << "USAGE: " << argv[0] << " -i INPUT -m METH [OPTIONS]\n";
     std::cout << desc << '\n';
     exit(!vm.count("help"));
   }
 
-  d4::MethodManager *method = d4::MethodManager::makeMethodManager(vm, std::cout);
-  method->run(vm); 
+  d4::MethodManager *method =
+      d4::MethodManager::makeMethodManager(vm, std::cout);
+  method->run(vm);
   delete method;
 
   return 0;
-}// main
+} // main
--- src/heuristics/cnf/PartitioningHeuristicBipartiteDual.cpp
+++ src/heuristics/cnf/PartitioningHeuristicBipartiteDual.cpp
@@ -1,27 +1,26 @@
 /*
-* d4 
-* Copyright (C) 2020  Univ. Artois & CNRS
-* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-#include <bitset>
+ * d4
+ * Copyright (C) 2020  Univ. Artois & CNRS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <algorithm>
+#include <bitset>
 
 #include "PartitioningHeuristicBipartiteDual.hpp"
 
-namespace d4
-{
+namespace d4 {
 /**
    Constructor.
 
@@ -29,17 +28,13 @@ namespace d4
    @param[in] _om, a structure manager.
 */
 PartitioningHeuristicBipartiteDual::PartitioningHeuristicBipartiteDual(
-    po::variables_map &vm,
-    WrapperSolver &_s,
-    SpecManager &_om) :
-    PartitioningHeuristicBipartiteDual(vm, _s, _om,
-                                         dynamic_cast<SpecManagerCnf&>(_om).getNbClause(),
-                                         dynamic_cast<SpecManagerCnf&>(_om).getNbVariable(),
-                                         dynamic_cast<SpecManagerCnf&>(_om).getSumSizeClauses())
-{  
+    po::variables_map &vm, WrapperSolver &_s, SpecManager &_om)
+    : PartitioningHeuristicBipartiteDual(
+          vm, _s, _om, dynamic_cast<SpecManagerCnf &>(_om).getNbClause(),
+          dynamic_cast<SpecManagerCnf &>(_om).getNbVariable(),
+          dynamic_cast<SpecManagerCnf &>(_om).getSumSizeClauses()) {
 } // constructor
 
-
 /**
    Constructor.
 
@@ -47,23 +42,19 @@ PartitioningHeuristicBipartiteDual::PartitioningHeuristicBipartiteDual(
    @param[in] _om, a structure manager.
 */
 PartitioningHeuristicBipartiteDual::PartitioningHeuristicBipartiteDual(
-    po::variables_map &vm,
-    WrapperSolver &s,
-    SpecManager &om,
-    int nbClause,
-    int nbVar,
-    int sumSize) :
-    PartitioningHeuristicBipartite(vm, om, s, nbClause, nbVar, sumSize)
-{
+    po::variables_map &vm, WrapperSolver &s, SpecManager &om, int nbClause,
+    int nbVar, int sumSize)
+    : PartitioningHeuristicBipartite(vm, om, s, nbClause, nbVar, sumSize) {
   // initialize the vector.
   m_partition.resize(m_nbClause + 1, 0);
 
   m_pm = PartitionerManager::makePartitioner(vm, m_nbClause, m_nbVar, sumSize);
-  m_hypergraph.init(m_nbVar + m_nbClause + sumSize + 1);  
+  m_hypergraph.init(m_nbVar + m_nbClause + sumSize + 1);
   m_hypergraphExtractor = new HyperGraphExtractorDual(m_nbVar, m_nbClause);
-  
-  m_staticPartitioner = PartitioningHeuristicStatic::makePartitioningHeuristicStatic(
-      vm, s, om, nbClause, nbVar, sumSize, "dual");
+
+  m_staticPartitioner =
+      PartitioningHeuristicStatic::makePartitioningHeuristicStatic(
+          vm, s, om, nbClause, nbVar, sumSize, "dual");
 } // constructor
 
-} // d4
+} // namespace d4
