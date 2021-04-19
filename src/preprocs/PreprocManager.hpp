4321ee65ddf2cab5375c1064bd46da4f5dc02153
Lagniez Jean-Marie <jmarie.lagniez@univ-artois.fr>
2021-04-19 09:37:21 +0200

    some refactoring to consider the preprocessing regarding the problem

--- src/preprocs/PreprocManager.hpp
+++ src/preprocs/PreprocManager.hpp
@@ -1,45 +1,39 @@
 /*
-* d4
-* Copyright (C) 2020  Univ. Artois & CNRS
-* 
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef d4_src_preprocs_PreprocManager_hpp
-#define d4_src_preprocs_PreprocManager_hpp
-
-#include <vector>
+ * d4
+ * Copyright (C) 2020  Univ. Artois & CNRS
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
 #include <boost/program_options.hpp>
+#include <vector>
 
-#include "src/problem/ProblemTypes.hpp"
 #include "src/problem/ProblemManager.hpp"
+#include "src/problem/ProblemTypes.hpp"
 
-namespace d4
-{
+namespace d4 {
 namespace po = boost::program_options;
-class PreprocManager
-{
- private:
- public:
+class PreprocManager {
+private:
+public:
   static PreprocManager *makePreprocManager(po::variables_map &vm,
                                             std::ostream &out);
 
-  virtual ~PreprocManager(){}
+  virtual ~PreprocManager() {}
 
-  /* The preprocessing is directly applied on pin and the result is save in pout  */
+  /* The preprocessing is directly applied on pin and the result is save in pout
+   */
   virtual ProblemManager *run(ProblemManager &pin) = 0;
 };
-}
-
-#endif
+} // namespace d4
