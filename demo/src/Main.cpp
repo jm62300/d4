89aa783a0684b417802db7ef23ecc5fef67b5f77
lagniez <jmlagniez@gmail.com>
2021-11-16 13:09:35 +0100

    some modification

--- demo/src/Main.cpp
+++ demo/src/Main.cpp
@@ -23,36 +23,24 @@
    The main function!
 */
 int main(int argc, char **argv) {
+  // to create the vm option.
   namespace po = boost::program_options;
-  namespace mpz = boost::multiprecision;
-
   po::options_description desc{"Options"};
   desc.add_options()
 #include "option.dsc"
       ;
-
   po::variables_map vm;
-  po::store(parse_command_line(argc, argv, desc), vm);
+  char **fake = NULL;
+  po::store(parse_command_line(0, fake, desc), vm);
 
-  try {
-    po::notify(vm);
-  } catch (const po::error &ex) {
-    std::cerr << ex.what() << '\n';
-    exit(1);
-  }
-
-  // help or problem with the command line
-  if (vm.count("help")) {
-    std::cout << desc << '\n';
-    exit(!vm.count("help"));
-  }
-
+  // to create the CNF instance.
   unsigned nbVar = 3;
   d4::ProblemManagerCnf problem;
   problem.setNbVar(nbVar);
 
   std::vector<double> &weightLit = problem.getWeightLit();
   std::vector<double> &weightVar = problem.getWeightVar();
+
   weightLit.resize((nbVar + 1) << 1, 1);
   weightVar.resize(nbVar + 1, 2);
 
@@ -64,19 +52,20 @@ int main(int argc, char **argv) {
 
   d4::LastBreathPreproc lastBreath;
   d4::PreprocManager *preproc =
-      d4::PreprocManager::makePreprocManager(vm, std::cout);
+      d4::PreprocManager::makePreprocManager(vm, std::cerr);
   d4::ProblemManager *preprocProblem = preproc->run(problem, lastBreath);
 
-  boost::multiprecision::mpf_float::default_precision(50);
+  namespace mpz = boost::multiprecision;
+  mpz::mpf_float::default_precision(50);
   d4::Counter<mpz::mpf_float> *method =
       d4::Counter<mpz::mpf_float>::makeCounter(vm, preprocProblem, "counting",
-                                               true, 50, std::cout, lastBreath);
+                                               true, 50, std::cerr, lastBreath);
 
   std::vector<d4::Var> setOfVar;
   for (unsigned i = 1; i <= nbVar; i++)
     setOfVar.push_back(i);
 
-  mpz::mpf_float v = method->count(setOfVar, std::cout);
+  mpz::mpf_float v = method->count(setOfVar, std::cerr);
   std::cout << "s " << v << "\n";
   delete method;
   return 0;
