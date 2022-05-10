ce0d0f405c47795c94ba8d039033a494748f9265
lagniez <lagniez@picsou.univ-artois.fr>
2022-05-10 10:49:31 +0200

    update clean

--- clean.sh
+++ clean.sh
@@ -8,6 +8,15 @@ then
     rm -f 3rdParty/glucose-3.0/utils/*.o*
     rm -rf 3rdParty/kahypar/build
     rm -rf 3rdParty/reducer/build
+
+    curRep=$PWD
+    cd 3rdParty/bipe/
+    ./clean.sh 1
+    cd $curRep
+
+    cd 3rdParty/reducer
+    ./clean.sh 1
+    cd $curRep
 fi
 
 rm -rf build/
