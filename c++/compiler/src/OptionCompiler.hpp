e0c9c2c0407b02f212b6e031d2b4833745e408ed
jmarie.lagniez <jmarie.lagniez@univ-artois.fr>
2026-05-26 17:38:53 +0200

    some comments

--- c++/compiler/src/OptionCompiler.hpp
+++ c++/compiler/src/OptionCompiler.hpp
@@ -8,19 +8,45 @@
 
 namespace d4 {
 
+/**
+ * @brief Group of options specific to the compiler demo.
+ *
+ * This class handles command-line options for dumping the compiled
+ * decision-DNNF to a file and for performing queries against it.
+ */
 class OptionCompiler : public OptionGroup {
  public:
+  /**
+   * @brief Construct a new OptionCompiler group.
+   *
+   * @param name The name of the option group (defaults to "compiler").
+   * @param description A brief description of the group's purpose.
+   */
   OptionCompiler(const std::string& name = "compiler",
                  const std::string& description = "Counter specific options")
       : OptionGroup(name, description) {}
 
+  /**
+   * @brief Path to the file where the compiled decision-DNNF should be dumped.
+   * Defaults to "/dev/null" (no dump).
+   */
   Option<std::string> dumpFile{"dump-file", "Output format", "/dev/null"};
+
+  /**
+   * @brief Path to a file containing queries to be executed on the compiled
+   * form. Defaults to "/dev/null" (no queries).
+   */
   Option<std::string> queryFile{"query-file", "Output style", "/dev/null"};
 
+  /**
+   * @brief Returns a list of all options contained in this group.
+   * @return A vector of pointers to the internal options.
+   */
   std::vector<OptionBase*> getAllOptions() override {
     return {&dumpFile, &queryFile};
   }
 
+  /** @brief Overload for printing the state of the compiler options. */
   friend std::ostream& operator<<(std::ostream& out, const OptionCompiler& dt) {
     out << " Option Compiler: dump-file(" << dt.dumpFile.get()
         << ") query-file(" << dt.queryFile.get() << ")\n";
